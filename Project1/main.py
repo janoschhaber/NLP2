8a6a6df13d59747db353fa5a7c24fbd08b4da1d3
janoschhaber <janoschhaber@freenet.de>
2017-04-12 13:02:05 +0200

    Clean Upload to start on Viterbi

--- Project1/main.py
+++ Project1/main.py
@@ -16,7 +16,6 @@ __status__ = "First Draft"
 
 import numpy as np
 import time
-from nltk.translate import AlignedSent
 from nltk.translate import Alignment
 from nltk.translate import metrics
 
@@ -70,8 +69,8 @@ def get_trans_prob(e_word, f_word, trans_probs, nr_e_words):
     :param nr_e_words: total number of encountered english words
     :return: conditional probability p(e_word|f_word)
     """
-    if trans_probs.has_key(f_word):
-        if trans_probs[f_word].has_key(e_word):
+    if f_word in trans_probs:
+        if e_word in trans_probs[f_word]:
             # print("Found: Probability of {} given {} is {}".format(e_word, f_word, trans_probs[f_word][e_word]))
             return trans_probs[f_word][e_word]
         else:
@@ -80,7 +79,10 @@ def get_trans_prob(e_word, f_word, trans_probs, nr_e_words):
             f_prob_mass = 0
             for entry in trans_probs[f_word].values():
                 f_prob_mass += entry
-            # print("Not Found: Probability of {} given {} is {} with a remaining probability mass of {} and {} words not assigned yet ".format(e_word, f_word, float(1 - f_prob_mass) / f_missing, float(1 - f_prob_mass), f_missing))
+            if float(1 - f_prob_mass) / f_missing == 0:
+                print("Not Found: Probability of {} given {} is {} with a remaining probability mass of {} and {} words not assigned yet ".format(e_word, f_word, float(1 - f_prob_mass) / f_missing, float(1 - f_prob_mass), f_missing))
+                print trans_probs[f_word].values()
+                return float(1) / nr_e_words
             return float(1 - f_prob_mass) / f_missing
     else:
         # print("No Key: Probability of {} given {} is {}".format(e_word, f_word, float(1) / nr_e_words))
@@ -98,54 +100,65 @@ def get_perplexity(e_pred_sent, f_sent, trans_probs, nr_e_words):
     """
     perplexity = 0
     for index, e_pred in enumerate(e_pred_sent):
+        print ("Probability for {} given {} is {}".format(e_pred, f_sent[index],
+        get_trans_prob(e_pred, f_sent[index], trans_probs, nr_e_words)))
         perplexity += np.log2(get_trans_prob(e_pred, f_sent[index], trans_probs, nr_e_words))
     return perplexity
 
 
-def align_sentences(f_sents, f_dict, e_sents, e_dict, trans_probs):
+def get_likelihood(e_pred_sent, f_sent, trans_probs, nr_e_words):
+    """
+    Returns a predicted translation's likelihood given the current model
+    :param e_pred_sent: the predicted translation
+    :param f_sent: the original sentence
+    :param trans_probs: the current model (translation probabilities p(e_word|f_word)) = trans_probs[f_word][e_word] = probability
+    :param nr_e_words: total number of english words encountered in training
+    :return: a predicted translation's likelihood given the current model
+    """
+    # TODO: Check for underflow
+    likelihood = 1
+    for index, e_pred in enumerate(e_pred_sent):
+        likelihood *= get_trans_prob(e_pred, f_sent[index], trans_probs, nr_e_words)
+    return likelihood
+
+
+def align_sentences(e_sents, f_sents, e_dict, trans_probs):
     """
     Translates a given set of sentences using the specified model
-    :param f_sents: the set of french sentences
-    :param f_dict: string to integer index dictionary for french
+    :param v_aligned: zipped list of aligned validation set sentences
     :param e_dict: string to integer index dictionary for english
+    :param f_dict: string to integer index dictionary for french
     :param trans_probs: the current model (translation probabilities p(e_word|f_word)) = trans_probs[f_word][e_word] = probability
-    :return: a list of translations
+    :return: a zipped list of translations and alignments
     """
     nr_e_words = len(e_dict)
-    translations = [None] * len(e_sents)
+    translations = [None] * len(f_sents)
+    alignments = [None] * len(f_sents)
 
     aligned = zip(e_sents, f_sents)
-    for pair_id,  pair in enumerate(aligned):
-        e_sent = [word.lower() for word in pair[0].split()]
-        f_sent = [word.lower() for word in pair[1].split()]
+    for pair_id, pair in enumerate(aligned):
+        e_sent = pair[0]
+        print e_sent
+        f_sent = pair[1]
+        print f_sent
+        sent_pred_trans = [None] * len(f_sent)
         sent_alignment = [None] * len(f_sent)
 
-        f_index = 0
-        for f_word in f_sent:
-            if INDEX_WORDS:
-                if f_dict.has_key(f_word):
-                    f_word = f_dict[f_word]
-                else:
-                    f_word = -1
-
+        for f_index, f_word in enumerate(f_sent):
+            print("Translating {}".format(f_word))
             e_probs = np.zeros(len(e_sent))
-            e_index = 0
-            for e_word in e_sent:
-                if INDEX_WORDS:
-                    if e_dict.has_key(e_word):
-                        e_word = e_dict[e_word]
-                    else:
-                        e_word = -1
+            for e_index, e_word in enumerate(e_sent):
                 e_probs[e_index] = get_trans_prob(e_word, f_word, trans_probs, nr_e_words)
-                e_index += 1
-            sent_alignment[f_index] = e_sent[np.argmax(e_probs)]
-            f_index += 1
+                print("Probability for {} is {}".format(e_word, e_probs[e_index]))
+            sent_pred_trans[f_index] = e_sent[np.argmax(e_probs)]
+            sent_alignment[f_index] = (f_index+1, np.argmax(e_probs)+1)
 
-        translations[pair_id] = sent_alignment
-    return translations
+        translations[pair_id] = sent_pred_trans
+        alignments[pair_id] = sent_alignment
+    return zip(translations, alignments)
 
 
-def evaluate_model(f_sents, f_dict, e_sents, e_dict, trans_probs, gold_alignments):
+def evaluate_model(e_sents, f_sents, e_sents_orig, e_dict, e_dict_inv, trans_probs, gold_alignments):
     """
     Returns the current model performance in terms of translation perplexity
     :param f_sents: the set of french sentences
@@ -157,24 +170,34 @@ def evaluate_model(f_sents, f_dict, e_sents, e_dict, trans_probs, gold_alignment
     nr_e_words = len(e_dict)
 
     sent_perplexities = np.zeros(len(f_sents))
+    sent_likelihoods = np.zeros(len(f_sents))
     sent_AERs = np.zeros(len(f_sents))
-    translations = align_sentences(f_sents, f_dict, e_sents, e_dict, trans_probs)
-    for index, e_pred_sent in enumerate(translations):
-        print ("Predicted alignment: {}".format(e_pred_sent))
-        e_pred_sent = index_sentence(e_pred_sent, e_dict)
-        f_sent_indexed = index_sentence([word.lower() for word in f_sents[index].split()], f_dict)
-        sent_perplexities[index] = get_perplexity(e_pred_sent, f_sent_indexed, trans_probs, nr_e_words)
 
-        # TODO: Implement AER calculation
-        f_sent = [word.lower() for word in f_sents[index].split()]
-        ref = Alignment([(0, 0), (1, 1), (2, 2)])
-        test = Alignment([(0, 0), (1, 2), (2, 1)])
-        print ( metrics.alignment_error_rate(ref, test))
+    model_output = align_sentences(e_sents, f_sents, e_dict, trans_probs)
+    for index, pair in enumerate(model_output):
+        e_pred_sent = pair[0]
+        f_sent = f_sents[index]
+        alignment = pair[1]
 
-    return [-np.sum(sent_perplexities), sum(sent_AERs)/len(sent_AERs)]
+        print ("Predicted translation: {}".format(decode_sentence(e_pred_sent, e_dict_inv)))
+        print ("Actual translation: {}".format(e_sents_orig[index]))
+        print ("Alignment: {}".format(alignment))
+        print ("Gold standard alignment: {}".format(gold_alignments[index]))
+
+        sent_perplexities[index] = get_perplexity(e_pred_sent, f_sent, trans_probs, nr_e_words)
+        sent_likelihoods[index] = get_likelihood(e_pred_sent, f_sent, trans_probs, nr_e_words)
+
+        # TODO: Fix AER
+        # ref = Alignment([(0, 0), (1, 1), (2, 2)])
+        # test = Alignment([(0, 0), (1, 2), (2, 1)])
+        # print metrics.alignment_error_rate(ref, test)
+
+        sent_AERs[index] = 1 # metrics.alignment_error_rate(gold_alignments[index], alignment)
+
+    return [-np.sum(sent_perplexities), sum(sent_likelihoods)/len(sent_likelihoods), sum(sent_AERs)/len(sent_AERs)]
 
 
-def index_sentence(sentence, dictionary):
+def encode_sentence(sentence, dictionary):
     """
     Returns the indexed version of the input sentence
     :param sentence: a string list sentence
@@ -182,15 +205,29 @@ def index_sentence(sentence, dictionary):
     """
     encoded = np.zeros(len(sentence))
     for index, word in enumerate(sentence):
-        if INDEX_WORDS:
-            if dictionary.has_key(word):
-                encoded[index] = dictionary[word]
-            else:
-                encoded[index] = -1
+        if word in dictionary:
+            encoded[index] = dictionary[word]
+        else:
+            encoded[index] = -1
     return encoded
 
 
-def produce_output(f_sents, f_dict, e_sents, e_dict, trans_probs, filename='results.dat'):
+def decode_sentence(sentence, dictionary):
+    """
+    Returns the decoded version of the input sentence
+    :param sentence: an indexed list sentence
+    :return: the decoded list of strings sentence
+    """
+    decoded = [None] * len(sentence)
+    for index, word in enumerate(sentence):
+        if word in dictionary:
+            decoded[index] = dictionary[word]
+        else:
+            decoded[index] = 'NULL'
+    return decoded
+
+
+def produce_output(v_e_sents, v_f_sents, e_dict, f_dict, e_inv_dict, trans_probs, gold_alignments, performances, filename='results.dat'):
     """
     Produces a translation output for the specified set of sentences
     :param f_sents: the set of french sentences
@@ -201,22 +238,23 @@ def produce_output(f_sents, f_dict, e_sents, e_dict, trans_probs, filename='resu
     :param filename:
     :return:
     """
-    nr_e_words = len(e_dict)
+    if INDEX_WORDS:
+        e_sents = [encode_sentence(sent, e_dict) for sent in v_e_sents]
+        f_sents = [encode_sentence(sent, f_dict) for sent in v_f_sents]
+    else:
+        e_sents = v_e_sents
+        f_sents = v_f_sents
 
-    sent_perplexities = np.zeros(len(f_sents))
-    translations = align_sentences(f_sents, f_dict, e_sents, e_dict, trans_probs)
-    for index, e_pred_sent in enumerate(translations):
-        e_pred_sent = index_sentence(e_pred_sent, e_dict)
-        f_sent = index_sentence([word.lower() for word in f_sents[index].split()], f_dict)
-        sent_perplexities[index] = get_perplexity(e_pred_sent, f_sent, trans_probs, nr_e_words)
-
-    perplexity = -np.sum(sent_perplexities)
-    result = zip(f_sents, translations, e_sents)
-
+    model_output = align_sentences(e_sents, f_sents, e_dict, trans_probs)
     f = open(filename, 'w')
-    f.write("Model perplexity: {}\n\n".format(perplexity))
-    for trio in result:
-        f.write("{}was translated as \n{} \ngold standard translation: \n{}\n\n".format(trio[0], " ".join(trio[1]), trio[2]))
+    f.write("Model perplexity: {}\n".format(performances[0]))
+    f.write("Average sentence likelihood: {}\n".format(performances[1]))
+    f.write("Average sentence AER: {}\n\n".format(performances[2]))
+    for i, output in enumerate(model_output):
+        translation = output[0]
+        alignment = output[1]
+        f.write("{}\nwas translated as \n{} \ngold standard translation: \n{}\n Predicted alignment: \n{}\n True alignment: \n{}\n\n"
+                .format(v_f_sents[i], decode_sentence(translation, e_inv_dict), v_e_sents[i], alignment, gold_alignments[i]))
     f.close()
 
 
@@ -243,6 +281,8 @@ def get_gold_alignments(filename = 'validation/dev.wa.nonullalign'):
 
 def main():
     start = time.time()
+
+    # Read training data
     e_lines = read_file('training/hansards.36.2.e')
     f_lines = read_file('training/hansards.36.2.f')
     if len(f_lines) != len(e_lines):
@@ -251,105 +291,61 @@ def main():
 
     train_size = len(f_lines)
     print ("Train set size: {} sentences".format(train_size))
+    if TRAIN_LIMIT > 0:
+        e_lines = e_lines[0:TRAIN_LIMIT]
+        f_lines = f_lines[0:TRAIN_LIMIT]
     print ("Limited to {} sentences for debugging".format(TRAIN_LIMIT))
 
-    v_f_lines = read_file('validation/dev.f')
-    v_e_lines = read_file('validation/dev.e')
-    v_f_lines = v_f_lines[0:VAL_LIMIT]
-    v_e_lines = v_e_lines[0:VAL_LIMIT]
-    if len(f_lines) != len(e_lines):
-        print("ERROR: Validation data not aligned properly!")
-        quit()
-    print ("Validation set size: {} sentences".format(len(v_f_lines)))
-    gold_alignments = get_gold_alignments()
-
-
     e_words = set(word.lower() for line in e_lines for word in line.split())
-    e_dict = wordlist2dict(e_words.union("NULL"))
-    e_inv_dict = inverse_dict(e_dict)
-    f_words = set(word.lower() for line in f_lines for word in line.split())
-    f_dict = wordlist2dict(f_words)
     nr_e_words = len(e_words)
+    e_dict = wordlist2dict(e_words) #.union("NULL"))
+    e_dict_inv = inverse_dict(e_dict)
+    f_words = set(word.lower() for line in f_lines for word in line.split())
     nr_f_words = len(f_words)
+    f_dict = wordlist2dict(f_words)
+    f_dict_inv = inverse_dict(f_dict)
     if VERBOSE: print "The english dictionary contains {} words, the french one {}".format(nr_e_words, nr_f_words)
 
-    aligned = zip(e_lines, f_lines)
+    e_sents = [[word.lower() for word in line.split()] for line in e_lines]
+    f_sents = [[word.lower() for word in line.split()] for line in f_lines]
+    if INDEX_WORDS:
+        e_sents = [encode_sentence(sent, e_dict) for sent in e_sents]
+        f_sents = [encode_sentence(sent, f_dict) for sent in f_sents]
 
-    trans_probs = {}
-    e_f_counts = {}
-    total = {}
+    aligned = zip(e_sents, f_sents)
 
-    last_perplexity = None
-    #while True:
-    for i in range(0,ITERATIONS):
-        counter = 0
-        for pair in aligned:
-            e_sent = [word.lower() for word in pair[0].split()]
-            f_sent = [word.lower() for word in pair[1].split()]
-            if VERBOSE: print e_sent, f_sent
+    # Read validation data
+    v_e_lines = read_file('validation/dev.e')
+    v_f_lines = read_file('validation/dev.f')
+    if VAL_LIMIT > 0:
+        v_e_lines = v_e_lines[0:VAL_LIMIT]
+        v_f_lines = v_f_lines[0:VAL_LIMIT]
+    if len(e_lines) != len(f_lines):
+        print("ERROR: Validation data not aligned properly!")
+        quit()
+    print ("Validation set size: {} sentence(s)".format(len(v_f_lines)))
 
-            # TODO: Add the zero word to the English sentence
+    v_e_sents = [[word.lower() for word in line.split()] for line in v_e_lines]
+    v_f_sents = [[word.lower() for word in line.split()] for line in v_f_lines]
 
-            if VERBOSE: print("Compute Normalization")
-            s_total = np.zeros(len(e_sent))
-            e_index = 0
-            for e_word in e_sent:
-                if INDEX_WORDS: e_word = e_dict[e_word]
-                for f_word in f_sent:
-                    if INDEX_WORDS: f_word = f_dict[f_word]
-                    s_total[e_index] = get_trans_prob(e_word, f_word, trans_probs, nr_e_words)
-                e_index += 1
-            if VERBOSE: print s_total
+    if INDEX_WORDS:
+        v_e_sents_encoded = [encode_sentence(sent, e_dict) for sent in v_e_sents]
+        v_f_sents_encoded = [encode_sentence(sent, f_dict) for sent in v_f_sents]
+    else:
+        v_e_sents_encoded = v_e_sents
+        v_f_sents_encoded = v_f_sents
 
-            if VERBOSE: print("Collect Counts")
-            e_index = 0
-            for e_word in e_sent:
-                if INDEX_WORDS: e_word = e_dict[e_word]
-                for f_word in f_sent:
-                    if INDEX_WORDS: f_word = f_dict[f_word]
+    gold_alignments = get_gold_alignments()
 
-                    # Update Counts
-                    if e_f_counts.has_key(f_word):
-                        if e_f_counts[f_word].has_key(e_word):
-                            current_count = e_f_counts[f_word][e_word]
-                            e_f_counts[f_word][e_word] = current_count + get_trans_prob(e_word, f_word, trans_probs, nr_e_words) / s_total[e_index]
-                        else:
-                            e_f_counts[f_word][e_word] = get_trans_prob(e_word, f_word, trans_probs, nr_e_words) / s_total[e_index]
-                    else:
-                        e_f_counts[f_word] = {e_word : get_trans_prob(e_word, f_word, trans_probs, nr_e_words) / s_total[e_index]}
-                        # if VERBOSE: print e_f_counts[f_word][e_word]
-                    # Update F Total
-                    if total.has_key(f_word):
-                        current_count = total[f_word]
-                        total[f_word] = current_count + get_trans_prob(e_word, f_word, trans_probs, nr_e_words) / s_total[e_index]
-                    else:
-                        total[f_word] = get_trans_prob(e_word, f_word, trans_probs, nr_e_words) / s_total[e_index]
-                e_index += 1
+    # TODO: Fix AER
+    # ref = Alignment([(0, 0), (1, 1), (2, 2)])
+    # test = Alignment([(0, 0), (1, 2), (2, 1)])
+    # print metrics.alignment_error_rate(ref, test)
 
-            counter += 1
-            if counter % 1000 == 0 : print("{}/{}".format(counter, TRAIN_LIMIT))
-            if counter == TRAIN_LIMIT : break
-
-        if VERBOSE: print("Estimate Probabilities")
-        for f_word in total.keys():
-            for e_word in e_f_counts[f_word].keys():
-                if trans_probs.has_key(f_word):
-                    trans_probs[f_word][e_word] = e_f_counts[f_word][e_word] / total[f_word]
-                    # if VERBOSE: print e_f_counts[f_word][e_word]
-                else:
-                    trans_probs[f_word] = {e_word : e_f_counts[f_word][e_word] / total[f_word]}
-                    # if VERBOSE: print e_f_counts[f_word][e_word]
-
-        [perplexity, AER] = evaluate_model(v_f_lines, f_dict, v_e_lines, e_dict, trans_probs, gold_alignments)
-        print ("Perplexity: {}".format(perplexity))
-        print ("AER: {}".format(AER))
-        if last_perplexity is None: last_perplexity = perplexity
-        # elif perplexity / last_perplexity > 0.5: break
-        last_perplexity = perplexity
-
-    produce_output(v_f_lines, f_dict, v_e_lines, e_dict, trans_probs)
     end = time.time()
     print("Execution time: {}".format(end - start))
+
+    print f_dict_inv[754]
 
 
 if __name__ == "__main__":
